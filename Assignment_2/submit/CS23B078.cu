45a69d89104d05604705753387248e813bbc93a4
Akaezist <samvkrish@gmail.com>
2026-09-06 10:51:44 +0530

    Naive method update

--- Assignment_2/submit/CS23B078.cu
+++ Assignment_2/submit/CS23B078.cu
@@ -77,24 +77,24 @@ void compute(int p, int q, int r, int *h_matrixA, int *h_matrixB,
 	cudaMalloc(&t_matD, r * q * sizeof(int));
 	cudaMalloc(&d_matrixTemp, p * r * sizeof(int));
 	
-	debugPrint("A", d_matrixA, p, q);
+	// debugPrint("A", d_matrixA, p, q);
 	transpose<<<q, p>>>(d_matrixA, t_matA);
-	debugPrint("AT", t_matA, p, q);
+	// debugPrint("AT", t_matA, p, q);
 
-	debugPrint("D", d_matrixA, r, q);
+	// debugPrint("D", d_matrixA, r, q);
 	transpose<<<r, q>>>(d_matrixD, t_matD);
-	debugPrint("DT", t_matA, q, r);
+	// debugPrint("DT", t_matA, q, r);
 	
-	debugPrint("B", d_matrixB, q, r);
-	debugPrint("C", d_matrixC, p, q);
+	// debugPrint("B", d_matrixB, q, r);
+	// debugPrint("C", d_matrixC, p, q);
 	multiply<<<p, r>>>(t_matA, d_matrixB, q, d_matrixE);
-	debugPrint("E = ATB", d_matrixE, p, r);
+	// debugPrint("E = ATB", d_matrixE, p, r);
 
 	multiply<<<p, r>>>(d_matrixC, t_matD, q, d_matrixTemp);
-	debugPrint("Temp = CDT", d_matrixTemp, p, r);
+	// debugPrint("Temp = CDT", d_matrixTemp, p, r);
 
 	add<<<p, r>>>(d_matrixE, d_matrixTemp);
-	debugPrint("Final = E + Temp", d_matrixE, p, r);
+	// debugPrint("Final = E + Temp", d_matrixE, p, r);
 
 
 	cudaDeviceSynchronize();
