4c16b6c06899d69b3f365cc3998b41ef00d5f768
Akaezist <samvkrish@gmail.com>
2026-09-06 11:04:46 +0530

    Naive method update

--- Assignment_2/submit/CS23B078.cu
+++ Assignment_2/submit/CS23B078.cu
@@ -85,16 +85,16 @@ void compute(int p, int q, int r, int *h_matrixA, int *h_matrixB,
 	transpose<<<r, q>>>(d_matrixD, t_matD);
 	debugPrint("DT", t_matD, q, r);
 	
-	debugPrint("B", d_matrixB, q, r);
-	debugPrint("C", d_matrixC, p, q);
+	// debugPrint("B", d_matrixB, q, r);
+	// debugPrint("C", d_matrixC, p, q);
 	multiply<<<p, r>>>(t_matA, d_matrixB, q, d_matrixE);
-	debugPrint("E = ATB", d_matrixE, p, r);
+	// debugPrint("E = ATB", d_matrixE, p, r);
 
 	multiply<<<p, r>>>(d_matrixC, t_matD, q, d_matrixTemp);
-	debugPrint("Temp = CDT", d_matrixTemp, p, r);
+	// debugPrint("Temp = CDT", d_matrixTemp, p, r);
 
 	add<<<p, r>>>(d_matrixE, d_matrixTemp);
-	debugPrint("Final = E + Temp", d_matrixE, p, r);
+	// debugPrint("Final = E + Temp", d_matrixE, p, r);
 
 
 	cudaDeviceSynchronize();
