bfee991a40f84fc52e5d704c573634eb954882a5
Akaezist <samvkrish@gmail.com>
2026-09-09 08:56:25 +0530

    Debugging multiply

--- Assignment_2/submit/CS23B078.cu
+++ Assignment_2/submit/CS23B078.cu
@@ -19,13 +19,13 @@ __global__ void print(const char* name, int* mat, int r, int c) {
 
 void debugPrint(const char* label, int* d_mat, int r, int c) {
     char *d_label;
-	printf("Entered debug\n");
+	// printf("Entered debug\n");
     cudaMalloc(&d_label, strlen(label) + 1);
     cudaMemcpy(d_label, label, strlen(label) + 1, cudaMemcpyHostToDevice);
-	printf("Launching print of %s\n", label);
+	// printf("Launching print of %s\n", label);
     print<<<1, 1>>>(d_label, d_mat, r, c);
     cudaDeviceSynchronize();  
-	printf("Back from %s\n", label);
+	// printf("Back from %s\n", label);
     cudaFree(d_label);
 }
 
@@ -130,38 +130,35 @@ void compute(int p, int q, int r, int *h_matrixA, int *h_matrixB,
 	cudaMalloc(&t_matD, r * q * sizeof(int));
 	cudaMalloc(&d_matrixTemp, p * r * sizeof(int));
 	
-	debugPrint("A", d_matrixA, p, q);
+	// debugPrint("A", d_matrixA, p, q);
 	int row = (q + 31)/32, col = (p + 31)/32;
 	transpose<<<row*col, BLOCK>>>(d_matrixA, q, p, t_matA);
-	debugPrint("AT", t_matA, q, p);
+	// debugPrint("AT", t_matA, q, p);
 
-	debugPrint("D", d_matrixD, r, q);
+	// debugPrint("D", d_matrixD, r, q);
 	row = (r + 31)/32, col = (q + 31)/32;
 	transpose<<<row*col, BLOCK>>>(d_matrixD, r, q, t_matD);
-	debugPrint("DT", t_matD, q, r);
+	// debugPrint("DT", t_matD, q, r);
 	
-	debugPrint("B", d_matrixB, q, r);
-	debugPrint("C", d_matrixC, p, q);
+	// debugPrint("B", d_matrixB, q, r);
+	// debugPrint("C", d_matrixC, p, q);
 	cudaMemset(d_matrixE, 0, p*r*sizeof(int));
-	cudaDeviceSynchronize();
 
 	// multiply<<<p, r>>>(t_matA, d_matrixB, q, d_matrixE);
 	int x = (p + 15)/16, y = (q + 15)/16, z = (r + 15)/16;
 	multiply<<<dim3(x, y, z), 257>>>(t_matA, d_matrixB, p, q, r, d_matrixE);
-	cudaDeviceSynchronize();
-	debugPrint("E = ATB", d_matrixE, p, r);
+	// debugPrint("E = ATB", d_matrixE, p, r);
 	
 	cudaMemset(d_matrixTemp, 0, p*r*sizeof(int));
 	// multiply<<<p, r>>>(d_matrixC, t_matD, q, d_matrixTemp);
 	multiply<<<dim3(x, y, z), 257>>>(d_matrixC, t_matD, p, q, r, d_matrixTemp);
-	cudaDeviceSynchronize();
 
-	debugPrint("Temp = CDT", d_matrixTemp, p, r);
+	// debugPrint("Temp = CDT", d_matrixTemp, p, r);
 
 	add<<<p, r>>>(d_matrixE, d_matrixTemp);
 	cudaDeviceSynchronize();
 
-	debugPrint("Final = E + Temp", d_matrixE, p, r);
+	// debugPrint("Final = E + Temp", d_matrixE, p, r);
 
 
 	cudaDeviceSynchronize();
