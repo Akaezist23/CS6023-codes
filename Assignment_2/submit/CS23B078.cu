df56521fc264108d25d0b5cb96ec42bbe5534172
Akaezist <samvkrish@gmail.com>
2026-09-10 21:01:57 +0530

    Optimizing over new method 1

--- Assignment_2/submit/CS23B078.cu
+++ Assignment_2/submit/CS23B078.cu
@@ -100,10 +100,10 @@ __global__ void multiply(int* mat1, int* mat2, int p, int q, int r, int* res) { 
     tiles[idx2] = (act_r2 < r && act_c2 < q) ? mat2[act_r2 * q + act_c2] : 0; //we will pass the transpose here
 	
 	__syncthreads();
-	if (threadIdx.x == 0) {
-		for (int x = 0; x < 32; x++) {
-			for (int y = 0; y < 32; y++) {
-				int res_r = 32 * i + x, res_c = 32 * k + y;
+	// if (threadIdx.x == 0) {
+	// 	for (int x = 0; x < 32; x++) {
+	// 		for (int y = 0; y < 32; y++) {
+				int res_r = 32 * i + bl_r, res_c = 32 * k + bl_c;
 				if (res_r < p && res_c < r) {
 					int acc = 0;
 					for (int z = 0; z < 32; z++) {
@@ -112,10 +112,10 @@ __global__ void multiply(int* mat1, int* mat2, int p, int q, int r, int* res) { 
 					    acc += tiles[idx1] * tiles[idx2];
 					}
 					atomicAdd(&res[res_r * r + res_c], acc);
-				}
-			}
-		}
-	}
+	// 			}
+	// 		}
+	// 	}
+	// }
 }
 
 __global__ void add(int* mat1, int* mat2) { //we'll do this in-place, and just add everything to mat1
