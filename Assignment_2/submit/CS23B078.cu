bde66aa0ec2902d27b8a816b843bdadfe7d0c7e9
Akaezist <samvkrish@gmail.com>
2026-09-10 21:04:10 +0530

    Optimizing over new method 1

--- Assignment_2/submit/CS23B078.cu
+++ Assignment_2/submit/CS23B078.cu
@@ -103,16 +103,17 @@ __global__ void multiply(int* mat1, int* mat2, int p, int q, int r, int* res) { 
 	// if (threadIdx.x == 0) {
 	// 	for (int x = 0; x < 32; x++) {
 	// 		for (int y = 0; y < 32; y++) {
-				int res_r = 32 * i + bl_r, res_c = 32 * k + bl_c;
+				int x = bl_r, y = bl_c;
+				int res_r = 32 * i + x, res_c = 32 * k + y;
 				if (res_r < p && res_c < r) {
 					int acc = 0;
 					for (int z = 0; z < 32; z++) {
-					    int idx1 = bl_r * 32 + z;        
-					    int idx2 = 1024 + bl_c * 32 + z;  
+					    int idx1 = x * 32 + z;        
+					    int idx2 = 1024 + y * 32 + z;  
 					    acc += tiles[idx1] * tiles[idx2];
 					}
 					atomicAdd(&res[res_r * r + res_c], acc);
-	// 			}
+				}
 	// 		}
 	// 	}
 	// }
